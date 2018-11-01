af85c0e863a6da696d1aba5ebf583e38945bc059
alumno <alumno>
2018-10-31 22:23:12 -0300

    ejercicio 17

---
+++ Ejercicios17/Ejercicio17/Boligrafo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio17
+{
+    public class Boligrafo
+    {
+        public const short cantidadTintaMaxima=100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(short tinta, ConsoleColor color)
+        {
+            this.tinta = tinta;
+            this.color = color;
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        public bool Pintar(int gasto, out string dibujo)
+        {
+            dibujo = "";
+
+            short nivelTinta = (short)(this.tinta - gasto);
+            if (nivelTinta >= 0)
+            {
+                for (int i = 0; i < gasto; i++)
+                {
+                    dibujo = dibujo + "*";
+                }
+                Console.WriteLine("{0}", dibujo);
+                SetTinta(nivelTinta);
+                return true;
+            }
+            return false;
+        }
+
+        public void Recargar()
+        {
+            short tintaActual = GetTinta();
+            short tintaNecesaria = (short)(cantidadTintaMaxima - tintaActual);
+            SetTinta(tintaNecesaria);
+        }
+
+        private void SetTinta(short tinta)
+        {
+            
+            if (tinta <= cantidadTintaMaxima && tinta >= -cantidadTintaMaxima)
+            {
+                short auxTinta = (short)(cantidadTintaMaxima + tinta);
+                if (auxTinta >= 0 && auxTinta <= cantidadTintaMaxima)
+                    this.tinta = auxTinta;
+            }
+            else
+                Console.WriteLine("No es posible asignar esa cantidad");
+        }
+    }
+}
---
+++ Ejercicios17/Ejercicio17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo azul = new Boligrafo(100,ConsoleColor.Blue);
+            Boligrafo rojo = new Boligrafo(50, ConsoleColor.Red);
+            string textoColor;
+
+            azul.Pintar(20, out textoColor);
+            Console.ReadKey();
+        }
+    }
+}
